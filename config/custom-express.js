aac3f7745912822ae240501d6a0eaf5b418ff228
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-05-10 21:54:15 -0300

    02) Funcionalidades básicas da API de Pagamentos - 05 Aplicando validações na entrada

--- config/custom-express.js
+++ config/custom-express.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 //retorna uma instancia do express
 module.exports = function(){
@@ -9,6 +10,7 @@ module.exports = function(){
 
     app.use(bodyParser.urlencoded({extended: true})); //
     app.use(bodyParser.json()); //faz parser de json, faz parser de um corpo de requisição
+    app.use(expressValidator()); //lib para validaçoes
 
     consign()
     .include('controllers') //a pasta controller deve ser carregada dentro da variável 'app' assim que subir 
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -9,27 +9,39 @@ module.exports = function (app) {
     });
 
     //receber os dados para criar o pagamento
-    app.post('/pagamentos/pagamento', function (req,res) {
+    app.post('/pagamentos/pagamento', function (req, res) {
 
-       //o pagamento é recebido pelo body
-       var pagamento =  req.body;
-       console.log('processando a requisicao de um novo pagamento');
+        //validação do json
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório").notEmpty();
+        req.assert("valor", "Valor é obrigatório e deve ser um decimal").notEmpty().isFloat();
+        var erros = req.validationErrors();
 
-       pagamento.status = 'CRIADO';
-       pagamento.data = new Date();
-
-       var connection = app.persistencia.connectionFactory(); //caminho do arquivo do connectionFactory, é invocada através do app. Aqui é instanciada a conexão com o banco
-       var pagamentoDAO = new app.persistencia.PagamentoDao(connection); //Criando uma nova instância do pagamentodao. Caminho do arquivo do PagamentoDao, é invocada através do app
-
-       pagamentoDAO.salva(pagamento, function(erro,resultado){
-
-        if(erro){
-            res.send(erro);
-        }else{
-           console.log('Pagamento criado');
-           res.json(pagamento)
+        if(erros){
+            console.log("Erros de validação encontrados");
+            res.status(400).send(erros);
+            return;
         }
-       });
+
+        //o pagamento é recebido pelo body
+        var pagamento = req.body;
+        console.log('processando a requisicao de um novo pagamento');
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date();
+
+        var connection = app.persistencia.connectionFactory(); //caminho do arquivo do connectionFactory, é invocada através do app. Aqui é instanciada a conexão com o banco
+        var pagamentoDAO = new app.persistencia.PagamentoDao(connection); //Criando uma nova instância do pagamentodao. Caminho do arquivo do PagamentoDao, é invocada através do app
+
+        pagamentoDAO.salva(pagamento, function (erro, resultado) {
+
+            if (erro) {
+                console.log('Erro ao inserir no banco:', erro);
+                res.status(400).send(erro);
+            } else {
+                console.log('Pagamento criado');
+                res.json(pagamento)
+            }
+        });
 
     });
 }
