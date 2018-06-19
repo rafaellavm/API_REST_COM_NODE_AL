e41d84905b8e33234581318d962c862851909dad
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-06-18 22:05:06 -0300

    04) Streaming de dados - 01) Lendo arquivos com buffer

--- payfast/servicos/CorreiosSOAPClient.js
+++ payfast/servicos/CorreiosSOAPClient.js
@@ -1,16 +1,19 @@
 var soap = require('soap');
 
-function CorreiosSOAPClient(){
+function CorreiosSOAPClient() {
   this._url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
 }
 
-module.exports = function(){
+module.exports = function () {
   return CorreiosSOAPClient;
 }
 
-CorreiosSOAPClient.prototype.calculaPrazo = function(args, callback){
-  soap.createClient(this._url, function(erro, cliente){
-              console.log('cliente soap criado');
-              cliente.CalcPrazo(args, callback);
-          });
+CorreiosSOAPClient.prototype.calculaPrazo = function (args, callback) {
+  
+  soap.createClient(this._url, function (erro, cliente) {
+    
+    console.log('cliente soap criado');
+    
+    cliente.CalcPrazo(args, callback);
+  });
 }
